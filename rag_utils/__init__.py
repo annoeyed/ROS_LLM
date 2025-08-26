61b93049df5950cc40d0347571c6c8d764857b62
annoeyed <nykim727@gmail.com>
2025-08-27 08:23:38 +0900

    docs(cleanup): update README; remove legacy files; resolve rag_utils/__init__ merge

--- rag_utils/__init__.py
+++ rag_utils/__init__.py
@@ -2,17 +2,34 @@
 # -*- coding: utf-8 -*-
 """
 RAG Utilities Module
+RAG(Retrieval-Augmented Generation) 관련 유틸리티들
 """
 
-from .ai_client import AIClient, OpenAIClient, AnthropicClient, MockAIClient, AIClientFactory
-from .config_loader import ConfigLoader
+# RAG 관련 핵심 모듈들
+from .config import Config
+from .cwe_rag import CWERAG
+from .cve_rag import CVERAG
+from .cwe_collector import CWECollector
+from .cve_validator import CVEValidator
+from .mitre_cwe_api import MITRECWEAPI
+from .nvd_api import NVDApi
+from .security_guidelines import SecurityGuidelines
+from .cwe_database import CWEDatabase
+from .build_ragbase import RAGBaseBuilder
+from .splitters import DocumentSplitter
+from .schema import DocumentSchema
 
 __all__ = [
-    'AIClient',
-    'OpenAIClient', 
-    'AnthropicClient',
-    'MockAIClient',
-    'AIClientFactory',
-    'ConfigLoader'
+    'Config',
+    'CWERAG',
+    'CVERAG', 
+    'CWECollector',
+    'CVEValidator',
+    'MITRECWEAPI',
+    'NVDApi',
+    'SecurityGuidelines',
+    'CWEDatabase',
+    'RAGBaseBuilder',
+    'DocumentSplitter',
+    'DocumentSchema'
 ]
-
